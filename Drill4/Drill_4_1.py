be6640e9336f547c8d8a74a2007cb405270c4fc0
Woo_Sj <wsj94472681@gmail.com>
2019-10-11 01:43:16 +0900

    NewRepository

---
+++ Drill4/Drill_4_1.py
@@ -0,0 +1,60 @@
+from pico2d import *
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+
+def handle_events():
+    global running
+    global dir_x, dir_y
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
+
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+running = True
+
+x = 800 // 2
+y = 600 // 2
+
+frame = 0
+dir_x = 0
+dir_y = 0
+arrow = 100
+
+while running:
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, arrow * 1, 100, 100, x, y)
+    update_canvas()
+
+    handle_events()
+    frame = (frame + 1) % 8
+    x += dir_x * 5
+    y += dir_y * 5
+close_canvas()
+
---
+++ Drill5/Drill_5.py
@@ -0,0 +1,61 @@
+from pico2d import *
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+def Follow_cursor():
+    global Following
+    global x, y
+    global cx,cy
+    global arrow
+    global click
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            Following = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.type == SDLK_ESCAPE:
+            Following = False
+
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            x, y = event.x - 20, KPU_HEIGHT - 1 - event.y + 20
+            click = True
+            if x < cx:
+                arrow = 0
+            elif x > cx:
+                arrow = 1
+        elif event.type == SDL_MOUSEBUTTONUP:
+            click = False
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+cursor = load_image('hand_arrow.png')
+character = load_image('animation_sheet.png')
+
+Following = True
+frame = 0
+hide_cursor()
+Click_Event = False
+arrow = 100
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+cx, cy = KPU_WIDTH // 2, KPU_HEIGHT // 2
+click = False
+
+while Following:
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    cursor.draw(x, y)
+    if click:
+        if x == cx and y == cy:
+            x = 0
+            y = 0
+        cx += (x - cx) / 100
+        cy += (y - cy) / 100
+
+    character.clip_draw(frame * 100, arrow * 100, 100, 100, cx, cy)
+
+    update_canvas()
+    frame = (frame + 1) % 8
+    Follow_cursor()
+
+close_canvas()
