a3eb732d8e007bd2cff5f408a1f6d77f50388ff1
Woo_Sj <wsj94472681@gmail.com>
2019-10-11 03:33:11 +0900

    Update Drill6_2.py

--- Drill6/Drill6_2.py
+++ Drill6/Drill6_2.py
@@ -1,7 +1,6 @@
 from pico2d import *
 import random
 
-
 # init
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -17,77 +16,74 @@ x1, y1 = KPU_WIDTH // 2, KPU_HEIGHT // 2
 
 move_location = []
 
+
 def curve_moving(points):
     global move_location
     global flag
 
-
+    # p0-p1
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[9], points[0], points[1], points[2])
         move_location.append(mr)
-
+    # p1-p2
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[0], points[1], points[2], points[3])
         move_location.append(mr)
-
+    # p2-p3
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[1], points[2], points[3], points[4])
         move_location.append(mr)
-
-
+    # p3-p4
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[2], points[3], points[4], points[5])
         move_location.append(mr)
-
+    # p4-p5
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[3], points[4], points[5], points[6])
         move_location.append(mr)
-
-
+    # p5-p6
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[4], points[5], points[6], points[7])
         move_location.append(mr)
-
+    # p6-p7
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[5], points[6], points[7], points[8])
         move_location.append(mr)
-
+    # p7-p8
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[6], points[7], points[8], points[9])
         move_location.append(mr)
-
+    # p8-p9
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[7], points[8], points[9], points[0])
         move_location.append(mr)
-
-
+    #p8-p9
     for i in range(0, 100, 2):
         t = i / 100
         mr = mathematics_type_b(t, points[8], points[9], points[0], points[1])
         move_location.append(mr)
 
 
-
-
 def mathematics_type_a(t, a, b, c):
-    mx = (2*t**2-3*t+1)*a[0]+(-4*t**2+4*t)*a[0]+(2*t**2-t)*a[0]
-    my = (2*t**2-3*t+1)*a[1]+(-4*t**2+4*t)*a[1]+(2*t**2-t)*a[1]
+    mx = (2 * t ** 2 - 3 * t + 1) * a[0] + (-4 * t ** 2 + 4 * t) * b[0] + (2 * t ** 2 - t) * c[0]
+    my = (2 * t ** 2 - 3 * t + 1) * a[1] + (-4 * t ** 2 + 4 * t) * b[1] + (2 * t ** 2 - t) * c[1]
     return mx, my
+
 
 def mathematics_type_b(t, a, b, c, d):
     mx = ((-t ** 3 + 2 * t ** 2 - t) * a[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * b[0] + (-3 * t ** 3 + 4 * t ** 2 + t) *
-             c[0] + (t ** 3 - t ** 2) * d[0]) / 2
+          c[0] + (t ** 3 - t ** 2) * d[0]) / 2
     my = ((-t ** 3 + 2 * t ** 2 - t) * a[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * b[1] + (-3 * t ** 3 + 4 * t ** 2 + t) *
-             c[1] + (t ** 3 - t ** 2) * d[1]) / 2
+          c[1] + (t ** 3 - t ** 2) * d[1]) / 2
     return mx, my
 
 
@@ -102,24 +98,24 @@ print(move_location)
 while running:
     clear_canvas()
 
-    if idx+1 == 499:
+    if idx + 1 == 499:
         idx = 0
 
     lx = move_location[idx][0]
-    lx2 = move_location[idx+1][0]
+    lx2 = move_location[idx + 1][0]
     ly = move_location[idx][1]
 
     if lx > lx2:
         arrow = 0
     else:
         arrow = 100
+
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.clip_draw(frame * 100, arrow, 100, 100, lx, ly)
     update_canvas()
     frame = (frame + 1) % 8
 
     delay(0.01)
-
 
     idx += 1
 
