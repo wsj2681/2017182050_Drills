960b9598aeee1ef6d52f1a9dcfa5c7a58b16a881
woosungjun <wsj94472681@gmail.com>
2019-10-18 10:27:16 +0900

    create Boy class

--- Drill7/Drill7.py
+++ Drill7/Drill7.py
@@ -1,1 +1,13 @@
 from pico2d import *
+import random
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
