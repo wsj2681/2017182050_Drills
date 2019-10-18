0bf352712e636db59380985e04d0d3d202673c27
woosungjun <wsj94472681@gmail.com>
2019-10-18 11:23:26 +0900

    fix and Ball Class update

--- Drill7/Drill7.py
+++ Drill7/Drill7.py
@@ -12,11 +12,33 @@ class Boy:
     def draw(self):
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
+
 class Grass:
     def __init__(self):
         self.image = load_image('grass.png')
     def draw(self):
         self.image.draw(400, 30)
+
+
+class Ball21:
+    def __init__(self):
+        self.x, self.y = random.randint(1, 799), 599
+        self.image = load_image('ball21x21.png')
+    def update(self):
+        self.y -= random.randint(1, 30)
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class Ball41:
+    def __init__(self):
+        self.x, self.y = random.randint(1, 799), 599
+        self.image = load_image('ball41x41.png')
+    def update(self):
+        self.y -= random.randint(1, 30)
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
 
 def handle_events():
     global running
@@ -37,10 +59,13 @@ while running:
     handle_events()
     for boy in team:
         boy.update()
+
     clear_canvas()
     grass.draw()
+
     for boy in team:
         boy.draw()
+
     update_canvas()
     delay(0.05)
 
