82a085afc56895dcb540a8f868be069cbc7c9d7b
Woo_Sj <wsj2681@naver.com>
2019-10-23 12:02:40 +0900

    title state update

--- Drill8/start_state.py
+++ Drill8/start_state.py
@@ -1,6 +1,6 @@
 import game_framework
 from pico2d import *
-
+import title_state
 
 name = "StartState"
 image = None
@@ -22,7 +22,8 @@ def update():
 
     if logo_time > 1.0:
         logo_time = 0
-        game_framework.quit()
+        # game_framework.quit()
+        game_framework.change_state(title_state)
     delay(0.01)
     logo_time += 0.01
 
--- Drill8/title_state.py
+++ Drill8/title_state.py
@@ -7,24 +7,29 @@ image = None
 
 
 def enter():
-    pass
+    global image
+    image = load_image('title.png')
 
 
 def exit():
-    pass
+    global image
+    del(image)
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
 
 
 def draw():
-    pass
-
-
-
-
-
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
 
 
 def update():
@@ -37,9 +42,3 @@ def pause():
 
 def resume():
     pass
-
-
-
-
-
-
