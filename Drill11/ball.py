5c7c5d05c171b89cd1ecbc733e761026a42927ce
Woo_Sj <wsj94472681@gmail.com>
2019-11-12 03:16:15 +0900

    ball. get_bb

--- Drill11/ball.py
+++ Drill11/ball.py
@@ -3,17 +3,17 @@ from pico2d import *
 import game_world
 import game_framework
 
+
 class Ball:
     image = None
 
     def __init__(self):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
-        self.x, self.y, self.fall_speed = random.randint(0, 1600-1), 60, 0
+        self.x, self.y, self.fall_speed = random.randint(0, 1600 - 1), 60, 0
 
     def get_bb(self):
-        # fill here
-        return 0,0,0,0
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
 
     def draw(self):
         self.image.draw(self.x, self.y)
@@ -22,8 +22,7 @@ class Ball:
     def update(self):
         self.y -= self.fall_speed * game_framework.frame_time
 
-    #fill here for def stop
-
+    # fill here for def stop
 
 # fill here
 # class BigBall
