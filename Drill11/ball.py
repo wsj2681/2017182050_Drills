8739430e29fd6d4a0c37494b8aa1abedabd2f48b
Woo_Sj <wsj94472681@gmail.com>
2019-11-12 03:19:37 +0900

    class big ball

--- Drill11/ball.py
+++ Drill11/ball.py
@@ -24,5 +24,18 @@ class Ball:
 
     # fill here for def stop
 
-# fill here
-# class BigBall
+
+class BigBall(Ball):
+    MIN_FALL_SPEED = 50  # 50 pps = 1.5 meter per sec
+    MAX_FALL_SPEED = 200  # 200 pps = 6 meter per sec
+    image = None
+
+    def __init__(self):
+        if BigBall.image is None:
+            BigBall.image = load_image('ball41x41.png')
+        self.x, self.y = random.randint(0, 1600 - 1), 500
+        self.fall_speed = random.randint(BigBall.MIN_FALL_SPEED,
+                                         BigBall.MAX_FALL_SPEED)
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
