0d5b3825fc420ae90f32297a95f01876be39af2b
Woo_Sj <wsj2681@naver.com>
2019-11-22 11:05:13 +0900

    zombie clear

--- Drill_Ai/zombie.py
+++ Drill_Ai/zombie.py
@@ -31,22 +31,22 @@ class Zombie:
                                        range(1, 11)]
 
     def __init__(self):
-        # positions for origin at top, left
-        positions = [(43, 750), (1118, 750), (1050, 530), (575, 220), (235, 33), (575, 220), (1050, 530), (1118, 750)]
+        position = [(43, 750), (1118, 750), (1060, 530), (575, 220), (235, 33), (575, 220), (1050, 530), (1118, 750)]
         self.patrol_positions = []
-        for p in positions:
-            self.patrol_positions.append((p[0], 1024 - p[1]))  # convert for origin at bottom, left
+        for p in position:
+            self.patrol_positions.append((p[0], 1024 - p[1]))
         self.patrol_order = 1
         self.target_x, self.target_y = None, None
         self.x, self.y = self.patrol_positions[0]
+
         self.load_images()
-        self.font = load_font('ENCR10B.TTF', 16)
         self.dir = random.random() * 2 * math.pi  # random moving direction
-        self.speed = 10
+        self.speed = 0
         self.timer = 1.0  # change direction every 1 sec when wandering
-        self.hp = 0.0
         self.frame = 0
         self.build_behavior_tree()
+        self.font = load_font('ENCR10B.TTF', 16)
+        self.zombie_hp = 1000
 
     def calculate_current_position(self):
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
@@ -56,79 +56,109 @@ class Zombie:
         self.y = clamp(50, self.y, 1024 - 50)
 
     def wander(self):
+        # fill here
         self.speed = RUN_SPEED_PPS
         self.calculate_current_position()
         self.timer -= game_framework.frame_time
         if self.timer < 0:
             self.timer += 1.0
             self.dir = random.random() * 2 * math.pi
+
         return BehaviorTree.SUCCESS
+        pass
 
     def find_player(self):
+        # fill here
         boy = main_state.get_boy()
         distance = (boy.x - self.x) ** 2 + (boy.y - self.y) ** 2
-
-        if distance < (PIXEL_PER_METER * 10) ** 2:
+        if distance < (PIXEL_PER_METER * 8) ** 2:
             self.dir = math.atan2(boy.y - self.y, boy.x - self.x)
             return BehaviorTree.SUCCESS
         else:
             self.speed = 0
             return BehaviorTree.FAIL
+        pass
+
+    def compare_hp(self):
+        boy = main_state.get_boy()
+        distance = (boy.x - self.x) ** 2 + (boy.y - self.y) ** 2
+        if distance < (PIXEL_PER_METER * 8) ** 2:
+            if boy.boy_hp >= self.zombie_hp:
+                self.dir = math.atan2(boy.x - self.x, boy.y - self.y)
+                return BehaviorTree.FAIL
+            else:
+                return BehaviorTree.SUCCESS
+        pass
 
     def move_to_player(self):
+        # fill here
         self.speed = RUN_SPEED_PPS
         self.calculate_current_position()
         return BehaviorTree.SUCCESS
+        pass
+
+    def find_ball(self):
+        ball = main_state.get_ball()
+        distance = (ball.x - self.x) ** 2 + (ball.y - self.y) ** 2
+        if distance < (PIXEL_PER_METER * 5) ** 2:
+            self.dir = math.atan2(ball.y - self.y, ball.x - self.x)
+            return BehaviorTree.SUCCESS
+        else:
+            self.speed = 0
+            return BehaviorTree.FAIL
+
+        pass
+
+    def move_to_ball(self):
+        self.speed = RUN_SPEED_PPS
+        self.calculate_current_position()
+        return BehaviorTree.SUCCESS
+        pass
 
     def get_next_position(self):
-        self.target_x, self.target_y = self.patrol_positions[self.patrol_order % len(self.patrol_positions)]
-        self.patrol_order = 1
+        # fill here
+        self.target_x, self.target_y = \
+            self.patrol_positions[self.patrol_order % len(self.patrol_positions)]
+        self.patrol_order += 1
         self.dir = math.atan2(self.target_y - self.y, self.target_x - self.x)
         return BehaviorTree.SUCCESS
+        pass
 
     def move_to_target(self):
+        # fill here
         self.speed = RUN_SPEED_PPS
         self.calculate_current_position()
-
         distance = (self.target_x - self.x) ** 2 + (self.target_y - self.y) ** 2
-
         if distance < PIXEL_PER_METER ** 2:
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.RUNNING
-
-    def find_ball(self):
-        balls = main_state.get_ball()
-        for ball in balls:
-            distance = (ball.x - self.x) ** 2 + (ball.y - self.y) ** 2
-
-            if distance < (PIXEL_PER_METER * 5) ** 2:
-                self.dir = math.atan2(ball.y - self.y, ball.x - self.x)
-                return BehaviorTree.SUCCESS
-            else:
-                self.speed = 0
-                return BehaviorTree.FAIL
-
-    def move_to_ball(self):
         pass
 
     def build_behavior_tree(self):
+        # fill here
         wander_node = LeafNode("Wander", self.wander)
+        compare_hp_node = LeafNode("Compare HP", self.compare_hp)
         find_player_node = LeafNode("Find Player", self.find_player)
         move_to_player_node = LeafNode("Move to Player", self.move_to_player)
+        chase_node = SequenceNode("Chase")
+        chase_node.add_children(compare_hp_node, find_player_node, move_to_player_node)
         find_ball_node = LeafNode("Find Ball", self.find_ball)
         move_to_ball_node = LeafNode("Move to Ball", self.move_to_ball)
-        chase_node = SequenceNode("Chase")
-        chase_node.add_children(find_player_node, move_to_player_node)
+        chase_ball_node = SequenceNode("ChaseBall")
+        chase_ball_node.add_children(find_ball_node, move_to_ball_node)
         wander_chase_node = SelectorNode("WanderChase")
-        wander_chase_node.add_children(chase_node, wander_node)
+        wander_chase_node.add_children(chase_node, chase_ball_node, wander_node)
         self.bt = BehaviorTree(wander_chase_node)
+        pass
 
     def get_bb(self):
         return self.x - 50, self.y - 50, self.x + 50, self.y + 50
 
     def update(self):
+        # fill here
         self.bt.run()
+        pass
 
     def draw(self):
         if math.cos(self.dir) < 0:
@@ -141,9 +171,7 @@ class Zombie:
                 Zombie.images['Idle'][int(self.frame)].draw(self.x, self.y, 100, 100)
             else:
                 Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 100, 100)
-        self.font.draw(self.x - 60, self.y + 50, '(HP: %3.2f)' % self.hp, (255, 255, 0))
-
-        draw_rectangle(*self.get_bb())
+        self.font.draw(self.x - 60, self.y + 50, '(HP: %3.2f)' % self.zombie_hp, (255, 255, 0))
 
     def handle_event(self, event):
         pass
