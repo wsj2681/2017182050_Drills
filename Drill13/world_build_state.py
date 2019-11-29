613b6d5f63fffa09774f40d254fb87c5e2b05c74
Woo_Sj <wsj2681@naver.com>
2019-11-29 11:15:01 +0900

    json.load

--- Drill13/world_build_state.py
+++ Drill13/world_build_state.py
@@ -12,13 +12,12 @@ import main_state
 from boy import Boy
 from zombie import Zombie
 
-
 boy = None
-
 
 name = "WorldBuildState"
 
 menu = None
+
 
 def enter():
     global menu
@@ -26,19 +25,22 @@ def enter():
     hide_cursor()
     hide_lattice()
 
+
 def exit():
     global menu
     del menu
 
+
 def pause():
     pass
+
 
 def resume():
     pass
 
+
 def get_boy():
     return boy
-
 
 
 def create_new_world():
@@ -46,9 +48,12 @@ def create_new_world():
     boy = Boy()
     game_world.add_object(boy, 1)
 
-    # fill here
+    with open('zombie_data.json', 'r') as f:
+        zombie_data_list = json.load(f)
 
-
+    for data in zombie_data_list:
+        zombie = Zombie(data['name'], data['x'], data['y'], data['size'])
+        game_world.add_object(zombie, 1)
 
 
 def load_saved_world():
@@ -57,14 +62,13 @@ def load_saved_world():
     # fill here
 
 
-
 def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-                game_framework.quit()
+            game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_n:
             create_new_world()
             game_framework.change_state(main_state)
@@ -72,16 +76,12 @@ def handle_events():
             load_saved_world()
             game_framework.change_state(main_state)
 
+
 def update():
     pass
 
+
 def draw():
     clear_canvas()
-    menu.draw(get_canvas_width()//2, get_canvas_height()//2)
+    menu.draw(get_canvas_width() // 2, get_canvas_height() // 2)
     update_canvas()
-
-
-
-
-
-
